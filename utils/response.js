a05ae04976e7cee2a77db5ca6f08c655771c95a6
Regedit-msc <efusanyaae@gmail.com>
2021-05-18 06:37:45 +0100

    new commit

--- utils/response.js
+++ utils/response.js
@@ -2,3 +2,4 @@ const createResponse = (type, message, data, token) => {
     return type === "bad" ? { status: type, error: message ?? "", data: {} } : { status: type, message: message ?? "", data: data ?? {}, token: token ?? "" };
 }
 module.exports = createResponse;
+//
